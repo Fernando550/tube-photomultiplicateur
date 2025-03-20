d0dbc1bd8bffa5bd2df607d686867d48336d325d
Fernando <FEOCM@ulaval.ca>
2025-03-20 18:52:41 -0400

    test of matplotlib

--- equipe-09-main.py
+++ equipe-09-main.py
@@ -1,7 +1,13 @@
+import matplotlib.pyplot as ptl
+
+
+squares = [1,2,3,4]
 
 def run():
-    print("hello world")
-    pass
+    fig, ax = ptl.subplots()
+    ax.plot(squares)
+
+    ptl.show()
 
 if __name__ == "__main__":
     run()

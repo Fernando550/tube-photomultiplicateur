f60b89b5e3256e4f6c899dbcd73ef664eaca9bda
Fernando <fernando26a@gmail.com>
2025-03-26 20:07:34 -0400

    ph_tube

--- equipe-09-main.py
+++ equipe-09-main.py
@@ -2,8 +2,13 @@ from ph_tube_class import ph_tube
 
 
 def run():
+<<<<<<< HEAD
     tube = ph_tube()
     tube.draw_photocathode()
+=======
+    print("hello world ")
+    pass
+>>>>>>> df8e702 (test of commit)
 
 if __name__ == "__main__":
     run()

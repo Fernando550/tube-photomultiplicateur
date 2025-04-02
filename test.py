0fd918c0d4bc9fccc37fb6c26e0b9e16f837b5af
Fernando <FEOCM@ulaval.ca>
2025-04-02 11:43:26 -0400

    lates vesion of ph

---
+++ test.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+
+
+x = np.linspace(-1, 1, 200)
+y = np.linspace(-1, 1, 200)
+X, Y = np.meshgrid(x, y)
+
+Z = X + Y  # Example scalar field
+
+plt.imshow(Z, cmap='hot', interpolation='none', extent=[-1, 1, -1, 1])
+plt.colorbar()
+plt.title("Heatmap of Z")
+plt.show()

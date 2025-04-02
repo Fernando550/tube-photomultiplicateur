fff22a45d4c5f1e0dad9add15d6e1bab0d3f9bb6
Fernando <fernando26a@gmail.com>
2025-04-02 16:08:28 -0400

    last commit

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ x = np.linspace(-1, 1, 200)
 y = np.linspace(-1, 1, 200)
 X, Y = np.meshgrid(x, y)
 
+tube = np.zeros()
+
 Z = X + Y  # Example scalar field
 
 plt.imshow(Z, cmap='hot', interpolation='none', extent=[-1, 1, -1, 1])

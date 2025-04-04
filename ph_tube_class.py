f747b161537c66affe320a039012d686172b3e59
Fernando <fernando26a@gmail.com>
2025-04-04 11:34:35 -0400

    test of the ph tube 2d version no functional

--- ph_tube_class.py
+++ ph_tube_class.py
@@ -18,23 +18,24 @@ class ph_tube:
         e : épaisseur d'une dynode (mm)
         f : largeur de la base du tube (mm)
         '''
-        self.N = N
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.e = e
-        self.f = f
+        self.n_dynodes = N
+        self.spacing = a
+        self.spacing_d_sides = b
+        self.dynode_width = c
+        self.spacing_dynodes = d
+        self.dynode_height = e
+        self.width = f
+        self.height = f/2
 
 
     def draw_photocathode(self):
         '''Affiche le tube graphiquement.'''
         fig, ax = plt.subplots()
-        tube = patches.Rectangle((0.5, 0.5), self.f, 4, edgecolor='black', facecolor='lightblue', linewidth=2)
+        tube = patches.Rectangle((0.5, 0.5), self.width, 4, edgecolor='black', facecolor='lightblue', linewidth=2)
         ax.add_patch(tube)
 
         # Set limits and aspect ratio
-        ax.set_xlim(0, self.f + 1)  # or self.f + 0.5 for tight fit
+        ax.set_xlim(0, self.width + 1)  # or self.f + 0.5 for tight fit
         ax.set_ylim(0, 5.5)         
         ax.set_aspect('equal')
 
@@ -42,7 +43,7 @@ class ph_tube:
         plt.show()
 
     def ph_model(self):
-        shape = (self.f, self.f)
+        shape = (self.width, self.height)
         ph = np.zeros(shape=shape)
 
         plt.imshow(ph, cmap='viridis')
--- test.py
+++ test.py
@@ -3,15 +3,46 @@ import matplotlib.patches as patches
 import numpy as np
 
 
-x = np.linspace(-1, 1, 200)
-y = np.linspace(-1, 1, 200)
-X, Y = np.meshgrid(x, y)
+# x = np.linspace(-1, 1, 200)
+# y = np.linspace(-1, 1, 200)
+# X, Y = np.meshgrid(x, y)
 
-tube = np.zeros()
+# tube = np.zeros()
 
-Z = X + Y  # Example scalar field
+# Z = X + Y  # Example scalar field
 
-plt.imshow(Z, cmap='hot', interpolation='none', extent=[-1, 1, -1, 1])
-plt.colorbar()
-plt.title("Heatmap of Z")
+# plt.imshow(Z, cmap='hot', interpolation='none', extent=[-1, 1, -1, 1])
+# plt.colorbar()
+# plt.title("Heatmap of Z")
+# plt.show()
+
+
+# Image dimensions
+height, width = 300, 600
+image = np.ones((height, width, 3))  # RGB white background
+
+# Draw a vertical gradient to simulate a glass tube effect
+for i in range(height):
+    alpha = 0.9 - 0.7 * (abs(i - height // 2) / (height // 2))
+    image[i, :, :] *= alpha
+
+fig, ax = plt.subplots()
+ax.imshow(image)
+
+# Dynode parameters
+n_dynodes = 8
+dynode_width = 60
+dynode_height = 10
+spacing = width // (n_dynodes + 2)
+
+from matplotlib.patches import Rectangle
+
+for i in range(n_dynodes):
+    x = spacing * (i + 1)
+    y = height // 2 - dynode_height // 2 - ((-1)**i) * 30  # Zig-zag pattern
+    dynode = Rectangle((x, y), dynode_width, dynode_height, color='skyblue', ec='black')
+    ax.add_patch(dynode)
+
+ax.set_title("2D Visualization of Photomultiplier Tube")
+ax.axis('off')
 plt.show()

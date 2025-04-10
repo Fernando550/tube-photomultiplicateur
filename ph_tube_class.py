24f7dc329ccd78502e687f1667213959039980db
Fernando <fernando26a@gmail.com>
2025-04-09 20:34:51 -0400

    Change arguments of the class by a list

--- ph_tube_class.py
+++ ph_tube_class.py
@@ -4,10 +4,7 @@ import numpy as np
 from matplotlib.patches import Rectangle
 
 
-class ph_tube:
-    '''Le tube photomultiplicateur!'''
-    def __init__(self, N=4, a=3, b=2, c=4, d=2, e=0.2, f=6, g=16):
-        '''
+'''
         Initialise le tube. 
 
         Arguments :
@@ -18,35 +15,59 @@ class ph_tube:
         d : espacement entre les dynodes (mm)
         e : épaisseur d'une dynode (mm)
         f : largeur de la base du tube (mm)
-        g : 
         '''
-        self.n_dynodes = N
-        self.spacing = a
-        self.spacing_d_sides = b
-        self.dynode_width = c
-        self.spacing_dynodes = d
-        self.dynode_height = e
-        self.width = g
-        self.height = f
-        self.h = 0.1
 
-    def make_grid_domain(self):
-        return (int(self.height/self.h), int(self.width/self.h))
+class ph_tube:
+    '''Le tube photomultiplicateur!'''
+    def __init__(self, **settings):
+
+        default = {
+            "n_dynodes": 4,
+            "spacing": 3,
+            "spacing_d_sides": 2,
+            "dynode_width": 4,
+            "spacing_dynodes": 2,
+            "dynode_height": 0.2,
+        }
+
+        default.update(settings)
+
+        self.width = 19
+        self.height = 6
+
+        # delta h
+        self.h = 0.1                         
+        
+
+        for key, value in default.items():
+            setattr(self, key, value)
+       
+
+        self.grid = np.zeros((int(self.height/self.h), int(self.width/self.h)))
+        print(np.shape(self.grid))
+
+    def transformPosition(self, x, y):
+        return (x/self.h, y/self.h)
+
+    def made_Dinode(self, x, y):
+        position = self.transformPosition(x, y)
+        dynode = Rectangle(position, self.dynode_width, self.dynode_height, color='black')
+        return dynode
 
     def draw_photocathode(self):
-        shape = self.make_grid_domain()
-        image = np.zeros(shape, dtype=float)  
         fig, ax = plt.subplots()
-        ax.imshow(image, cmap='plasma', origin='lower')
+        ax.imshow(self.grid, cmap='plasma', origin='lower',
+          extent=[0, self.width/self.h, 0, self.height/self.h])
 
-        for i in range(self.n_dynodes):
-            x = self.spacing * (i + 1)
-            y = self.height // 2 - self.dynode_height // 2 - ((-1)**i) * 30  # Zig-zag pattern
-            dynode = Rectangle((x, y), self.dynode_width, self.dynode_height, color='black')
-            ax.add_patch(dynode)
+        # for i in range(self.n_dynodes):
+        #     x = self.transformPosition()
+        #     dynode = self.made_Dinode()
+        #     ax.add_patch(dynode)
         
         ax.set_title("2D Visualization of Photomultiplier Tube")
         ax.axis('off')
+        ax.set_xlim(0, self.width/self.h)
+        ax.set_ylim(0, self.height/self.h)
         plt.show()
 
     def ph_visualaze(self):

1e97e95f0a3891fb3cba3a0f95c7859f3b6c0538
Fernando <FEOCM@ulaval.ca>
2025-04-19 14:30:54 -0400

    partucle motion add fuctions

--- ParticleMotionSolver.py
+++ ParticleMotionSolver.py
@@ -4,3 +4,12 @@ import numpy as np
 class particle_motion:
     def __init__(self):
         pass
+
+    def get_accelaration(self):
+        pass
+
+    def get_position(self):
+        pass
+
+    def draw_path(self):
+        pass
